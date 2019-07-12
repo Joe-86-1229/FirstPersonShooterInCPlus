74c55ea84337beb765c247a7ac227c0ebf9444e5
Joe-86 <jiujiu1229@gmail.com>
2019-07-11 17:18:55 -0700

    Create FirstPersonShooter.h

---
+++ FirstPersonShooter.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define SCREENWIDTH 120
+#define SCREENHEIGHT 40
+
+#define MAPWIDTH 16
+#define MAPHEIGHT 16
+
+float FOV = 3.14159f / 4.0f;	// Field of View
+float maxRenderDistance = 16.0f;
+
+float playerX = 8.0f;
+float playerY = 8.0f;
+float playerA = 0.f;	// player's view angle
